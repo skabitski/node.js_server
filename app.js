e228de51f42bcb82892d7195e04fc08a5f91b2eb
Maks_Skabitski <mskabitskiy@gmail.com>
2024-04-06 20:59:30 +0300

    added server

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const http = require("http");
+const express = require("express");
 
+const app = express();
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-  //localhost:3000/currenttime
-  //localhost:3000
-}
-
-const server = http.createServer(handleRequest);
-
-server.listen(3000);
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1");
+});
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+app.listen(3000);
